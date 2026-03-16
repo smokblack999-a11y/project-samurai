2e53b8f09138e663ab379c1d11bf2faf905cabf8
YOUR_NAME <YOUR_EMAIL>
2026-03-16 06:49:22 +0500

    fix main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.logger import log_info, log_ok
 from src.utils import now_str, banner
 
 
-def main() -> None:
+def main():
     log_info("Starting project...")
     log_ok(banner(Config.APP_NAME))
     log_info(f"Environment: {Config.APP_ENV}")
@@ -13,11 +13,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()from rich import print
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("[bold green]Samurai environment is ready[/bold green]")
-print("Project:", os.getcwd())
+    main()
