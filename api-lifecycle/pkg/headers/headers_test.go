1a3444611271fa74cf884014b4db0273e1ed40aa
Samurai <smokblack999@gmail.com>
2026-08-09 02:31:35 +0500

    style(api-lifecycle): gofmt header tests

--- api-lifecycle/pkg/headers/headers_test.go
+++ api-lifecycle/pkg/headers/headers_test.go
@@ -1,19 +1,23 @@
 package headers
 
 import (
-    "net/http"
-    "testing"
-    "time"
+	"net/http"
+	"testing"
+	"time"
 )
 
 func TestApplyAndParse(t *testing.T) {
-    h := make(http.Header)
-    dep := time.Unix(1754006400, 0).UTC()
-    sunset := time.Date(2026, 12, 1, 0, 0, 0, 0, time.UTC)
-    Apply(h, &dep, &sunset, "/v2/orders")
-    gotDep, err := ParseDeprecation(h.Get(Deprecation)); if err != nil { t.Fatal(err) }
-    if !gotDep.Equal(dep) { t.Fatalf("deprecation=%s want=%s", gotDep, dep) }
-    gotSunset, err := ParseSunset(h.Get(Sunset)); if err != nil { t.Fatal(err) }
-    if !gotSunset.Equal(sunset) { t.Fatalf("sunset=%s want=%s", gotSunset, sunset) }
-    if h.Get(Link) == "" { t.Fatal("missing successor-version Link") }
+	h := make(http.Header)
+	dep := time.Unix(1754006400, 0).UTC()
+	sunset := time.Date(2026, 12, 1, 0, 0, 0, 0, time.UTC)
+	Apply(h, &dep, &sunset, "/v2/orders")
+
+	gotDep, err := ParseDeprecation(h.Get(Deprecation))
+	if err != nil { t.Fatal(err) }
+	if !gotDep.Equal(dep) { t.Fatalf("deprecation=%s want=%s", gotDep, dep) }
+
+	gotSunset, err := ParseSunset(h.Get(Sunset))
+	if err != nil { t.Fatal(err) }
+	if !gotSunset.Equal(sunset) { t.Fatalf("sunset=%s want=%s", gotSunset, sunset) }
+	if h.Get(Link) == "" { t.Fatal("missing successor-version Link") }
 }
