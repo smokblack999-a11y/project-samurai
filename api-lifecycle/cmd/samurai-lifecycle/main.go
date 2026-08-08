1792975ccbb1b41d3270e92a34dd0c93ddfe69bc
Samurai <smokblack999@gmail.com>
2026-08-08 19:43:43 +0500

    feat(api-lifecycle): add lifecycle risk CLI

---
+++ api-lifecycle/cmd/samurai-lifecycle/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/smokblack999-a11y/project-samurai/api-lifecycle/pkg/lifecycle"
+	"github.com/smokblack999-a11y/project-samurai/api-lifecycle/pkg/risk"
+)
+
+func main() {
+	input := flag.String("input", "", "path to a lifecycle JSON record")
+	flag.Parse()
+	if *input == "" {
+		fmt.Fprintln(os.Stderr, "usage: samurai-lifecycle -input endpoint.json")
+		os.Exit(2)
+	}
+
+	data, err := os.ReadFile(*input)
+	if err != nil {
+		fatal(err)
+	}
+	var endpoint lifecycle.Endpoint
+	if err := json.Unmarshal(data, &endpoint); err != nil {
+		fatal(err)
+	}
+
+	result := risk.Evaluate(endpoint)
+	out, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		fatal(err)
+	}
+	fmt.Println(string(out))
+}
+
+func fatal(err error) {
+	fmt.Fprintln(os.Stderr, "error:", err)
+	os.Exit(1)
+}
